de3d1ff13b24cb9ef69ce9002c436bcae948804d
Wyatt Baggett <wyattbaggett@yahoo.com>
2025-12-18 14:30:52 -0500

    Enhances performance and browser compatibility.
    Improves application performance by optimizing CSS delivery and package imports. Configures browser support to target modern browsers, reducing the need for polyfills and decreasing bundle size. Also enhances image handling by allowing SVG and setting a content security policy for images.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Image optimization settings
+  experimental: {
+    cssChunking: 'strict',
+    optimizeCss: true,
+    optimizePackageImports: ['@fortawesome/react-fontawesome', '@fortawesome/free-solid-svg-icons', '@fortawesome/free-brands-svg-icons', '@fortawesome/free-regular-svg-icons'],
+  },
   images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: 'https',
